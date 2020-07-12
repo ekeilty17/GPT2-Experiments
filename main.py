f47f9da607943425365f40fe0486aa5a85097d61
Eric <epkeilty@hotmail.com>
2020-07-12 14:43:58 -0400

    error catching so it saves info

--- main.py
+++ main.py
@@ -39,39 +39,46 @@ def test_conditioning(model_tag, num_shots=3):
 
     output_df = pd.DataFrame(columns=['num_shots', 'prompt', 'response', 'original_reflection', 'label', 'new_reflection'])
 
-    for index, row in tqdm(df.iterrows()):
-        test_inp = ( str(row["prompt"]), str(row["response"]), str(row["reflection_gpt"]) )
-        test_label = int(row["gpt_valid_reflection"])
-
-        test_example = (test_inp, test_label)
-        
-        # this ensures the conditioned examples do not contain the test example
-        examples = [test_example]
-        while (test_example) in examples:
-            # we just keep resampling until we get a set without the test example
-            examples = get_n_examples(data, num_shots)
-        
-        # randomly shuffle so it's not all negative and then all positive
-        random.shuffle(examples)
+    try:
+        for index, row in tqdm(df.iterrows()):
             
-        # create final gpt2 input
-        primers = [convert_example_to_formatted_string(inp, label) for inp, label in examples]
-        test_str = convert_example_to_formatted_string(test_inp)
-        gpt2_input = '\n\n'.join(primers + [test_str])
+            if index > 1:
+                x = int("hi")
 
-        output = get_gpt2_output(model, tokenizer, device, gpt2_input)
-        new_reflection = get_reflection_from_gpt2_output(output)
+            test_inp = ( str(row["prompt"]), str(row["response"]), str(row["reflection_gpt"]) )
+            test_label = int(row["gpt_valid_reflection"])
+            test_example = (test_inp, test_label)
+            
+            # this ensures the conditioned examples do not contain the test example
+            examples = [test_example]
+            while (test_example) in examples:
+                # we just keep resampling until we get a set without the test example
+                examples = get_n_examples(data, num_shots)
+            
+            # randomly shuffle so it's not all negative and then all positive
+            random.shuffle(examples)
+                
+            # create final gpt2 input
+            primers = [convert_example_to_formatted_string(inp, label) for inp, label in examples]
+            test_str = convert_example_to_formatted_string(test_inp)
+            gpt2_input = '\n\n'.join(primers + [test_str])
 
-        prompt, response, original_reflection = test_inp
-        output_df = output_df.append({
-                            'num_shots': 2*num_shots,
-                            'prompt': prompt,
-                            'response': response,
-                            'original_reflection': original_reflection,
-                            'label': test_label,
-                            'new_reflection': new_reflection
-                        }, ignore_index=True)
-        
+            output = get_gpt2_output(model, tokenizer, device, gpt2_input)
+            new_reflection = get_reflection_from_gpt2_output(output)
+
+            prompt, response, original_reflection = test_inp
+            output_df = output_df.append({
+                                'num_shots': 2*num_shots,
+                                'prompt': prompt,
+                                'response': response,
+                                'original_reflection': original_reflection,
+                                'label': test_label,
+                                'new_reflection': new_reflection
+                            }, ignore_index=True)
+            
+    except Exception as e:
+        print(e)
+    
     return output_df
 
 if __name__ == "__main__":
@@ -85,5 +92,8 @@ if __name__ == "__main__":
                         help="Number of examples the model will be conditioned with")
     args = parser.parse_args()
     
+    print("begin testing")
     df = test_conditioning(args.model, args.num_shots)
+
+    print("saving to csv")
     df.to_csv('injecting_negative_examples.csv', index=False)
